dd98e1af5220ceffa4ae8f594460c5ecc89178ce
Odomontois <odomontois@gmail.com>
2014-07-17 00:54:43 +0400

    Monoid operations added
    Added possibility of monoid calculation in RBT

--- rbt-test.hs
+++ rbt-test.hs
@@ -4,35 +4,30 @@ import Rbt
 import Tree hiding (toList)
 import Data.List hiding (insert)
 import Control.Applicative
+import Data.Monoid
 
 pt m x = let i = insert x m in (i,i)
 
 
 check Leaf = True
-check (Node (RBData c val) left right) 
+check (Node (RBData c val _ _ ) left right) 
         | c == Red && (color left == Red || color right == Red) = error $ "Bad color at" ++ show val
         | any (>= val) $ toList left = error $ "Not sorted (left) at" ++ show val 
         | any (< val) $ toList right = error $ "Not sorted (right) at" ++ show val 
         | otherwise = check left && check right
 
-deep::RBT a -> Int
+deep::RBT a b-> Int
 deep t = Rbt.fold (0::Int) (\ _ l r -> 1 + (l `max` r)) t
 
-main = do 
-        let a::RBT Int
+main = do
+        --num <- read <$> getLine :: IO Int
+        let num = 1000
+        let a::RBT Int (Sum Int)
             a = rbt
-            num = 1000
-            b = foldr insert a [1..num]
+            b = foldr (uncurry insert) a ( [1..num] `zip` map Sum [1..num])
         print $ color b
-
-    --print b
-    --print $ check b
-    --print $ deep b
-    --let c = findSpot 1 $ zipper b
-    --print c
-    --let d = setTree (Node ( RBData Red 1 ) Leaf Leaf) $ c
-    --print d 
-    --let e = rollUp d  
-    --print e
-    --let f = stepUp d
-    --print f
+        print $ check b
+        print $ value b
+        print $ aggr b
+        print $ aggr $ leftTree b
+        print $ aggr $ rightTree b
--- tree.hs
+++ tree.hs
@@ -2,60 +2,74 @@ module Tree where
 
 import Prelude hiding (Left,Right)
 
-data Tree a = Node a (Tree a) (Tree a) | Leaf deriving Show
+data Tree a = Node{ nodeVal::a, leftTree::Tree a, rightTree:: Tree a} | Leaf deriving Show
 data Direction = Left | Right deriving (Show, Eq)
-data Zipper a =  Zipper{direction:: Direction, treeZ::Tree a, modified::Bool, prev::Zipper a} 
-                |Top  {                        treeZ::Tree a, modified::Bool } deriving Show
+type TF a = a -> Maybe a -> Maybe a -> a
+data Zipper a =  Zipper{direction:: Direction, treeZ::Tree a, modifiedZ::Bool, prevZ::Zipper a, transformZ::TF a} 
+                |Top  {                        treeZ::Tree a, modifiedZ::Bool                ,  transformZ::TF a}
 
 modValue:: (a -> a) ->Tree a->Tree a
 modValue _ Leaf = Leaf
 modValue f (Node x left right) = Node (f x) left right
 
-zipper::Tree a -> Zipper a
-zipper tree = Top tree False
+treeVal (Node x _ _)  = Just x
+treeVal Leaf        = Nothing
 
-isTop (Top _ _) = True
-isTop (Zipper _ _ _ _)  = False
+constTF = const . const 
+
+zipper::Tree a -> Zipper a
+zipper tree = Top tree False constTF
+zipperTF tree tf = Top tree False tf
+
+isTop (Top _ _ _) = True
+isTop (Zipper _ _ _ _ _)  = False
 
 goLeft::Zipper a -> Zipper a
-goLeft current = case (treeZ current) of  Node _ left _ -> Zipper Left left False current
+goLeft current = case (treeZ current) of  Node _ left _ -> Zipper Left left False current (transformZ current)
                                           Leaf          -> error "Couldn't go anywhere from leaf"
 
 goRight::Zipper a -> Zipper a
-goRight current = case (treeZ current) of  Node _ _ right -> Zipper Right right False current
+goRight current = case (treeZ current) of  Node _ _ right -> Zipper Right right False current (transformZ current)
                                            Leaf           -> error "Couldn't go anywhere from leaf"
 
-replace (Node x left right) modified direction = case direction of 
-    Left -> Node x modified right
-    Right -> Node x left modified
+replace (Node x left right) modified direction transform = 
+        let     (left', right') = case direction of 
+                        Left -> (modified, right)
+                        Right -> (left, modified)
+                x' = transform x (treeVal left') (treeVal right')    
+        in Node x' left' right'
 
 stepUp::Zipper a -> Zipper a
-stepUp (Top _  _) = error "Couldn't get higher than top"
-stepUp (Zipper _ _ False prev) = prev
-stepUp (Zipper direction tree True zipper) = let replaced = replace (treeZ zipper) tree direction in
-    case zipper of Zipper dir _ modif prev -> Zipper dir replaced True prev
-                   Top    _ modif          -> Top    replaced True
+stepUp (Top _ _ _) = error "Couldn't get higher than top"
+stepUp (Zipper _ _ False prev _) = prev
+stepUp (Zipper direction tree True zipper tf) = let replaced = replace (treeZ zipper) tree direction tf in
+    case zipper of Zipper dir _ modif prev tf -> Zipper dir replaced True prev tf
+                   Top    _ modif tf          -> Top    replaced True tf
 
 rollUp::Zipper a -> Tree a
-rollUp (Top tree _ ) = tree
-rollUp z@(Zipper _ _ _ _) = rollUp (stepUp z)
+rollUp (Top tree _ _) = tree
+rollUp z@(Zipper _ _ _ _ _) = rollUp (stepUp z)
+
+recalc::TF a -> Tree a -> Tree a
+recalc _ Leaf = Leaf
+recalc tf (Node x left right) = Node (tf x (treeVal left) (treeVal right)) left right
 
 modTree::(Tree a -> Tree a) -> Zipper a -> Zipper a
-modTree transform (Top node _ ) =  Top (transform node) True
-modTree transform (Zipper direction node _ prev) = Zipper direction (transform node) True prev
+modTree transform (Top node _ tf) =  Top (recalc tf $ transform node) True tf
+modTree transform (Zipper direction node _ prev tf) = Zipper direction (recalc tf $ transform node) True prev tf
 
 setTree tree = modTree (const tree)
 setValue x = let transform (Node _ l r) = Node x l r in modTree transform
 
 rotate::Zipper a -> Zipper a
-rotate zipper@(Top _ _ ) = zipper
-rotate (Zipper _ Leaf _ _) = error "Can not rotate Zipper"
-rotate (Zipper Left (Node c left center) modif zipper) =
+rotate zipper@(Top _ _ _ ) = zipper
+rotate (Zipper _ Leaf _ _ _ ) = error "Can not rotate Zipper"
+rotate (Zipper Left (Node c left center) modif zipper _) =
     let (Node p _ right ) = treeZ zipper in
     let modParent = Node p center right in 
     let modChild = Node c left modParent in 
     setTree modChild zipper
-rotate (Zipper Right (Node c center right) modif zipper) =
+rotate (Zipper Right (Node c center right) modif zipper _) =
     let (Node p left _) = treeZ zipper in
     let modParent = Node p left center in 
     let modChild = Node c modParent right in
